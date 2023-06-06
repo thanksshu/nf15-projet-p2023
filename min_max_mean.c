b79d787a72d5b481b169cff6ebf3a4c760ce5007
weisizu <sizu.wei@etu.utc.fr>
2023-06-06 10:52:49 +0200

    change the comments

--- min_max_mean.c
+++ min_max_mean.c
@@ -1,56 +1,49 @@
 #include "includes.h"
-double min(int length, double *temperature)
+double min(int length, double *temperatures)
 {
     int i; // compter the number
     double temperature_min;
 
-    temperature_min = *temperature;
+    temperature_min = *temperatures;
 
-    // get the min temperature
-
-    for (i = 1; i < length; ++i)
+    for (i = 1; i < length; ++i)    // get the min temperature
     {
-        if (*(temperature + i) < temperature_min)
+        if (*(temperatures + i) < temperature_min)
         {
-            temperature_min = *(temperature + i);
+            temperature_min = *(temperatures + i);
         }
     }
-    return temperature_min;
+    return temperature_min; // return the value of the min temperature
 }
 
-double max(int length, double *temperature)
+double max(int length, double *temperatures)
 {
     int i; // compter the number
     double temperature_max;
 
-    temperature_max = *temperature;
+    temperature_max = *temperatures;
 
-    // get the max temperature
-
-    for (i = 1; i < length; ++i)
+    for (i = 1; i < length; ++i)     // get the max temperature
     {
-        if (*(temperature + i) > temperature_max)
+        if (*(temperatures + i) > temperature_max)
         {
-            temperature_max = *(temperature + i);
+            temperature_max = *(temperatures + i);
         }
     }
-    return temperature_max;
+    return temperature_max; //return the value of the max temperature
 }
 
-double mean(int length, double *temperature)
+double mean(int length, double *temperatures)
 {
     int i; // compter the number
     double temperature_mean;
     double temperature_sum = 0;
 
-    // get the mean temperature
-
-    for (i = 0; i < length; ++i)
+    for (i = 0; i < length; ++i)    // get the mean temperature
     {
-        temperature_sum = temperature_sum + *(temperature + i);
-        temperature_mean = temperature_sum / length;
+        temperature_sum = temperature_sum + *(temperatures + i);
+        temperature_mean = temperature_sum / length; // calculate the value of the mean temperature
     }
-    return temperature_mean;
+    return temperature_mean; // get the value of the mean temperature
 }
-
 
