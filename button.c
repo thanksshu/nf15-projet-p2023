ab988003bc9d88b0175af866a962feae793f6cfc
weisizu <sizu.wei@etu.utc.fr>
2023-06-13 08:46:45 +0200

    add bouton description

--- button.c
+++ button.c
@@ -4,65 +4,61 @@ bool is_left_pressed_before = false;
 bool is_right_pressed_before = false;
 bool is_val_pressed_before = false;
 
-void init_button(void)
+void init_button(void)   // Set the initial state of the buttons
 {
-    // choix de la fonction GPIO pour le bouton P2.7(le bouton left)
-    P2->SEL0 &= ~BIT7; // on met P2SEL0 � 0 pour P2.7
-    P2->SEL1 &= ~BIT7; // on met P2SEL1 � 0 pour P2.7
+    P2->SEL0 &= ~BIT7; // Select GPIO function for left button(P2.7)
+    P2->SEL1 &= ~BIT7;
 
-    // choix de la fonction GPIO pour le bouton P10.4(le bouton down) et P10.5(le bouton right)
-    P10->SEL0 &= ~BIT4 & ~BIT5; // on met P10SEL0 � 0 pour P10.4 et P10.5
-    P10->SEL1 &= ~BIT4 & ~BIT5; // on met P10SEL1 � 0 pour P10.4 et P10.5
+    P10->SEL0 &= ~BIT4 & ~BIT5; // Select GPIO function for down button (P10.4) and right button (P10.5)
+    P10->SEL1 &= ~BIT4 & ~BIT5;
 
-    // choix de la fonction GPIO pour le bouton P2.6(le bouton up)
-    P2->SEL0 &= ~BIT6; // on met P2SEL0 � 0 pour P2.6
-    P2->SEL1 &= ~BIT6; // on met P2SEL1 � 0 pour P2.6
+    P2->SEL0 &= ~BIT6; // Select GPIO function for up button(P2.6)
+    P2->SEL1 &= ~BIT6;
 
-    // choix de la fonction GPIO pour le bouton P7.4(le bouton valid)
-    P7->SEL0 &= ~BIT4; // on met P7SEL0 � 0 pour P7.4
-    P7->SEL1 &= ~BIT4; // on met P7SEL1 � 0 pour P7.4
+    P7->SEL0 &= ~BIT4; // Select GPIO function for valid button (P7.4)
+    P7->SEL1 &= ~BIT4;
 
-    // reglage du bouton P2.7(le bouton left)
-    P2->DIR &= ~BIT7;     //P2.7 entr�e
-    P2->REN |= BIT7;      //R�sistance de pull autoris�e sur P2.7
-    P2->OUT |= BIT7;      //Pull-up sur P2.7
+    // Set the left button
+    P2->DIR &= ~BIT7;     // The left button is enter
+    P2->REN |= BIT7;      // Enable resistor
+    P2->OUT |= BIT7;      // Pull-up resistor
 
-    // reglage du bouton bouton P10.4(le bouton down) et P10.5(le bouton right)
-    P10->DIR &= ~BIT4;     //P10.4 entr�e
-    P10->REN |= BIT4;      //R�sistance de pull autoris�e sur P10.4
-    P10->OUT |= BIT4;      //Pull-up sur P10.4
+    // Set the down button and right button
+    P10->DIR &= ~BIT4;     // The down button is enter
+    P10->REN |= BIT4;      // Enable resistor
+    P10->OUT |= BIT4;      // Pull-up resistor
 
-    P10->DIR &= ~BIT5;     //P10.5 entr�e
-    P10->REN |= BIT5;      //R�sistance de pull autoris�e sur P10.5
-    P10->OUT |= BIT5;      //Pull-up sur P10.5
+    P10->DIR &= ~BIT5;     // The right button is enter
+    P10->REN |= BIT5;      // Enable resistor
+    P10->OUT |= BIT5;      // Pull-up resistor
 
-    // reglage du bouton P2.6(le bouton up)
-    P2->DIR &= ~BIT6;     //P2.6 entr�e
-    P2->REN |= BIT6;      //R�sistance de pull autoris�e sur P2.6
-    P2->OUT |= BIT6;      //Pull-up sur P2.6
+    // Set the up button
+    P2->DIR &= ~BIT6;     // The up button is enter
+    P2->REN |= BIT6;      // Enable resistor
+    P2->OUT |= BIT6;      // Pull-up resistor
 
-    // reglage du bouton P7.4(le bouton valid)
-    P7->DIR &= ~BIT4;     //P7.4 entr�e
-    P7->REN |= BIT4;      //R�sistance de pull autoris�e sur P7.4
-    P7->OUT |= BIT4;      //Pull-up sur P7.4
+    // Set the valid button
+    P7->DIR &= ~BIT4;     // The valid button is enter
+    P7->REN |= BIT4;      // Enable resistor
+    P7->OUT |= BIT4;      // Pull-up resistor
 }
 
-bool is_left_pressed()
+bool is_left_pressed() // Confirm that the left button is pressed
 {
-    bool result = false;
-    bool now = !((bool) (P2->IN & BIT7 ));
-    if (is_left_pressed_before == false && now == true)
+    bool result = false; // Define the initial state of the button as not pressed
+    bool now = !((bool) (P2->IN & BIT7 )); // Force the button state to be translated into a boolean type
+    if (is_left_pressed_before == false && now == true) // If the state of button was high and now is low, the button is pressed
     {
-        result = true;
+        result = true;  // Indicates that the left button is pressed
     }
     else
     {
-        result = false;
+        result = false; // Indicates that the left button is not pressed
     }
 
-    is_left_pressed_before = now;
+    is_left_pressed_before = now; // Transfer the current state to the previous state for the next comparison
 
-    return result;
+    return result;  // Return the state of the button
 }
 
 bool is_right_pressed()
